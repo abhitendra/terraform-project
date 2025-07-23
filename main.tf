714309235760d16c2945e32141e479a2e70d84e3
abhitendra747369@gmail.com <abhitendra747369@gmail.com>
2025-07-23 10:55:01 +0000

    latest

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "s3bucket" {
     acl    = "private"
  
     tags = {
-        Name        = "bucketterra"
+        Name        = "abhitendrabucketterra"
         Environment = "Dev"
     }
 }
